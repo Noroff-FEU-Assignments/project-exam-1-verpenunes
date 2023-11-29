70d81153c32f58b9f2e2df72fc7d4db09c90f245
verpenunes <106631829+verpenunes@users.noreply.github.com>
2023-11-29 21:56:16 +0100

    hamnurger menu

--- js/script.js
+++ js/script.js
@@ -67,4 +67,4 @@ window.sr = ScrollReveal({ reset: true});
 
 sr.reveal(".welcome", {duration: 2500});
 sr.reveal(".explore", {duration: 4400});
-sr.reveal(".connect", {duration: 4400});
+sr.reveal(".connect", {duration: 5400});
