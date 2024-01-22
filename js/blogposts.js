a26528b870f81c06eed6832fb1e62551c80335e9
verpenunes <106631829+verpenunes@users.noreply.github.com>
2024-01-22 12:33:32 +0100

    Loader

--- js/blogposts.js
+++ js/blogposts.js
@@ -1,5 +1,6 @@
 const blogsContainer = document.querySelector(".blogposts");
 const showMore = document.querySelector(".load-more");
+const loader = document.querySelector(".results");
 
 const baseUrl = "https://cors.noroff.dev/https://beyondborders.fun/wp-json/wp/v2/posts?per_page=10&_embed";
 const showMoreUrl = "https://cors.noroff.dev/https://beyondborders.fun/wp-json/wp/v2/posts?offset=10&_embed";
@@ -9,23 +10,29 @@ async function fetchBlogs(baseUrl) {
     const response = await fetch(baseUrl);
     const getBlogs = await response.json();
     console.log(getBlogs);
-    createHTML(getBlogs);
-  } catch(error) {
+    
+    const createHTML = getBlogs;
+
+    loader.innerHTML = `<div class="loader"></div>`;
+
+    setTimeout(function() {
+
+        loader.innerHTML = "";
+
+        createHTML.forEach(function(blogPost) {
+            blogsContainer.innerHTML += `
+            <div class="blogposts"><a href="blogdetails.html"id=${blogPost.id}><img src="${blogPost._embedded["wp:featuredmedia"]["0"].source_url}" alt="${blogPost.slug}"</a></div>
+            <div class="title">${blogPost.title.rendered}</div>
+            <div class="blogText">${blogPost.excerpt.rendered}</div>`;
+        });
+    },1000);
+  } 
+  catch(error) {
     console.log(error);
   }
 }
 
 fetchBlogs(baseUrl);
-
-function createHTML(posts) {
-    posts.forEach(function(blogPost) {
-        blogsContainer.innerHTML += `
-        <div class="blogposts"><a href="blogdetails.html"id=${blogPost.id}><img src="${blogPost._embedded["wp:featuredmedia"]["0"].source_url}" alt="${blogPost.slug}"</a></div>
-        <div class="title">${blogPost.title.rendered}</div>
-        <div class="blogText">${blogPost.excerpt.rendered}</div>
-                                    `;
-    });     
-}
 
 showMore.addEventListener("click", (e) => {
     e.preventDefault();
